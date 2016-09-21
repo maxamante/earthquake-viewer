2e7e18ca96f048445b12181d3ca64e83bf5b3a0f
Maxwell Amante <maxwellamante@gmail.com>
2016-09-21 08:57:15 -0700

    Leave pageLinks out for now; Use human readable time

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 // Flow
 
-// Let user know something is loading
-$('#main').html('<div class="loading">Loading quake data from the last 30 days...</div>');
-
 // - [ ] Make a request to USGS
 const requestEarthquakeData = function*(){
   let quakeEndpoint = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson';
@@ -14,17 +11,18 @@ const requestEarthquakeData = function*(){
 };
 
 const buildPageLinks = function(total) {
-  console.log(total);
   const entriesPerPage = 20;
   const numPages = Math.ceil(total / entriesPerPage);
-  let links = '';
+  let links = '<a href="#" class="backPage"><</a> ';
 
-  for (let i = 1; i < 11; i++) {
-    links += `<a href="#" class="pageLink">${i}</a> | `;
+  for (let i = 1; i < 10; i++) {
+    links += `<a href="#${i}" class="pageLink">${i}</a> | `;
   }
   if (numPages > 11) {
-    links += `... <a href="#" class="pageLink">${numPages}</a>`;
+    links += `<a href="#10" class="pageLink">10</a> ... <a href="#${numPages}" class="pageLink">${numPages}</a>`;
   }
+
+  links += ' <a href="#" class="nextPage">></a>';
 
   return links;
 };
@@ -35,11 +33,14 @@ const computeDate = function(timestamp) {
 
 // - [ ] Parse request
 const quakeData = requestEarthquakeData();
+
+//main()
+// Let user know something is loading
+$('#main').html('<div class="loading">Loading quake data from the last 30 days...</div>');
 quakeData.next().value.then(function(data) {
   let pageLinks = buildPageLinks(data['features'].length);
-  let quakes = `
-    <div class="pageLinks">${pageLinks}</div>
-    <div class="title">Earthquakes from the past 30 days:</div>`;
+  let quakes = `<div class="title">Earthquakes from the past 30 days:</div>`;
+
   for (let quake of data['features']) {
     let props = quake['properties'];
     let geo = quake['geometry'];
@@ -51,7 +52,7 @@ quakeData.next().value.then(function(data) {
         <ul>
           <li>id: <a href="${props['url']}">${quake['id']}</a></li>
           <li>place: ${props['place']}</li>
-          <li>time: ${props['time']}</li>
+          <li>time: ${new Date(props['time']).toLocaleTimeString()}</li>
           <li>magnitude: ${props['mag']}</li>
           <li>type: ${props['type']}</li>
           <li>coordinates: ${geo['coordinates']}</li>
@@ -62,22 +63,20 @@ quakeData.next().value.then(function(data) {
   // Show parsed quake data
   $('#main').html(quakes);
 
-  return new Promise((resolve) => {
-    // - [ ] Show detail on click
-    // First hide all details
-    $('.quakeDetails').hide();
-    // Then on click show details
-    $('.quakeEntry').each(function() {
-      $(this).click(function(event) {
-        let detailsId = '#' + event.target.children[0].id;
-        let isVisible = $(detailsId).is(':visible');
-        if (isVisible) {
-          $(detailsId).hide();
-        }
-        else {
-          $(detailsId).show();
-        }
-      })
-    });
+  // - [ ] Show detail on click
+  // First hide all details
+  $('.quakeDetails').hide();
+  // Then on click show details
+  $('.quakeEntry').each(function() {
+    $(this).click(function(event) {
+      let detailsId = '#' + event.target.children[0].id;
+      let isVisible = $(detailsId).is(':visible');
+      if (isVisible) {
+        $(detailsId).hide();
+      }
+      else {
+        $(detailsId).show();
+      }
+    })
   });
 });
